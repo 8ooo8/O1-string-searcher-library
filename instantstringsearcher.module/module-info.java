dea23f1fc1e1c3a70a221c4d7529a95f99d7cda3
8ooo8 <ben.cky.workspace@gmail.com>
2021-05-19 20:02:50 +0800

    modularize library

---
+++ instantstringsearcher.module/module-info.java
@@ -0,0 +1,5 @@
+module instantstringsearcher.module {
+    exports github.eightoooeight.instantstringsearcher;
+    exports github.eightoooeight.instantstringsearcher.trienode;
+    exports github.eightoooeight.instantstringsearcher.rawdata;
+}
